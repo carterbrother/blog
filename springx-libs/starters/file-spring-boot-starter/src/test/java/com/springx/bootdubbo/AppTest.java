5cfc66380a6dd493ce717de947370b41bca49579
lifuchun <fuchun.li@lifesense.com>
2019-06-06 15:07:33 +0800

    工具类抽取

---
+++ springx-libs/starters/file-spring-boot-starter/src/test/java/com/springx/bootdubbo/AppTest.java
@@ -0,0 +1,20 @@
+package com.springx.bootdubbo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
