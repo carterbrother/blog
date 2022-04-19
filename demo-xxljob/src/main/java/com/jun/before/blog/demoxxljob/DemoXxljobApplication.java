8f2c101ea2cdc291c5636540aa6f5c5e2d9ab916
Carter.li <505847426@qq.com>
2022-04-19 09:19:33 +0800

    init

---
+++ demo-xxljob/src/main/java/com/jun/before/blog/demoxxljob/DemoXxljobApplication.java
@@ -0,0 +1,13 @@
+package com.jun.before.blog.demoxxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoXxljobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoXxljobApplication.class, args);
+	}
+
+}
---
+++ demo-xxljob/src/test/java/com/jun/before/blog/demoxxljob/DemoXxljobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.before.blog.demoxxljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoXxljobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
